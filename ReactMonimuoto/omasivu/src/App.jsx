0d0167bedb42a6ef90bdd6a02f2f5915c61ac17c
Ville Heikkiniemi <ville.heikkiniemi@gmail.com>
2025-02-14 19:17:03 +0200

    react monimuoto

---
+++ ReactMonimuoto/omasivu/src/App.jsx
@@ -0,0 +1,21 @@
+import Header from "./components/Header.jsx";
+
+const Footer = () => {
+  return (
+      <footer>
+          <p>&copy; 2025 My Single Page</p>
+      </footer>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  )
+}
+
+
+export default App
---
+++ ReactMonimuoto/part1/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+return (
+    <div className="container">
+    <h1>Tervetuloa React-sivulle!</h1>
+    <p>Tämä on yksinkertainen React-sivu, jossa käytetään CSS:ää.</p>
+    </div>
+);
+}
+
+export default App;
